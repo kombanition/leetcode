01692885333419805d885d1ca2ebf1a07d45d3fa
kombanition <61285738+kombanition@users.noreply.github.com>
2024-09-20 16:38:58 +0200

    [2980] [Time Beats: 38.92%] [Memory Beats: 40.79%] - LeetPush

---
+++ 2980-Check-if-Bitwise-OR-Has-Trailing-Zeros.py
@@ -0,0 +1,12 @@
+import itertools as it
+
+class Solution:
+    def hasTrailingZeros(self, nums: List[int]) -> bool: 
+        count = 0
+        for number in nums:
+            if bin(number)[2:][-1] == '0':
+                count += 1      
+        if count >= 2:
+            return True
+        else:
+            return False
